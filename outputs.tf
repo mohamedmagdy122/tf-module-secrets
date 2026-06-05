9eef7e07c75d9f379bf011568382f8594906c85b
mohamedmagdy122 <your-email@gmail.com>
2026-06-05 05:56:38 +0300

    Fix: proper HCL syntax

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
-output "secret_arn" { value = aws_secretsmanager_secret.db.arn }
-output "secret_name" { value = aws_secretsmanager_secret.db.name }
-output "db_password" { value = random_password.db.result; sensitive = true }
+output "secret_arn" {
+  value = aws_secretsmanager_secret.db.arn
+}
+output "secret_name" {
+  value = aws_secretsmanager_secret.db.name
+}
+output "db_password" {
+  value     = random_password.db.result
+  sensitive = true
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
-variable "name" { type = string }
-variable "environment" { type = string }
-variable "recovery_window_in_days" { type = number; default = 0 }
+variable "name" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "recovery_window_in_days" {
+  type    = number
+  default = 0
+}
